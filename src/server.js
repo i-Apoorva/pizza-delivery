9556a7e1eed24931151f1d80da61fc5cee529028
i-Apoorva <apoorva.ambhoj95@gmail.com>
2020-01-24 19:26:08 +0000

    jwt and bcrypt

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 var express = require('express');
 var app  = express(); 
-var port = process.env.PORT || 8000;
 var bodyParser = require('body-parser');
+const logger = require('morgan');
 var mongoose   = require('mongoose'); 
-const axios = require('axios'); 
+const axios = require('axios');
+const router = express.Router();
 var routes = require('./routes/router');
+const environment = process.env.NODE_ENV; // development
+const stage = require('./config')[environment];
 var mongoDB =String(process.env.DB_URL);
 
 app.use(bodyParser.urlencoded({useNewUrlParser: true, extended: true} ));
 app.use(bodyParser.json());
+
+if (environment !== 'production') {
+  app.use(logger('dev'));
+}
 
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}); // connect to our database
 var db = mongoose.connection;
@@ -24,5 +31,8 @@ app.get('/', function(req, res) {
 
 app.use('/api/', routes);
 
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(`${stage.port}`, () => {
+  console.log(`Server now listening at localhost:${stage.port}`);
+});
+
+module.exports = app;
